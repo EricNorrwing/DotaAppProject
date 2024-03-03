e38afcf0ea74dab19d77b7b857c7e4cf111cc5c7
EricNorrwing <eric.norrwing@gmail.com>
2024-03-03 12:32:43 +0100

    first commit

---
+++ src/main/java/com/ericnorrwing/dotaappproject/DotaAppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ericnorrwing.dotaappproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotaAppProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DotaAppProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ericnorrwing/dotaappproject/DotaAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericnorrwing.dotaappproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotaAppProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
